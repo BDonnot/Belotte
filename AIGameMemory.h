a9fbf2b8c101f8fd2a4d91f64083a0bd5d2727ec
Benjamin DONNOT <benjamin.donnot@gmail.com>
2015-01-17 21:55:38 +0100

    starting memory implmentation

--- AIGameMemory.h
+++ AIGameMemory.h
@@ -1,6 +1,6 @@
 #ifndef AIGAMEMEMORY_H
 #define AIGAMEMEMORY_H
-
+#include <array>
 #include "Definitions.h"
 #include "Basic_Game_Info.h"
 
@@ -10,27 +10,55 @@ This class handles the memory of the AI players. It will store (hopefully in an 
 all the informations a player need to take the right decision.
 This class worries only about the playing phase of the game.
 **/
-
-class MemorizeCards //just an efficient way to store all the fallen cards.
+template<Uint n>
+class BoolStorage
 {
     protected :
-        bool _allCards[32];
+        bool _information[n];
     public :
-        MemorizeCards(){}
+        BoolStorage(){Reset();}
         void Reset()
         {
-            for (Uint i = 0; i < 32; i++)
+            for(Uint i = 0; i < n ; i++)
             {
-                _allCards[i] = false;
+                _information[i] = false;
             }
         }
+    protected :
+        const bool& getInfo(Uint i) const
+        {
+            return _information[i];
+        }
+        void setInformation(Uint index)
+        {
+            _information[index] = true;
+        }
+};
+
+class MemorizeCards : public BoolStorage<32> //just an efficient way to store all the fallen cards.
+{
+    public :
+        MemorizeCards(){}
         const bool & IsFallen(CARDS_COLOR color, CARDS_HEIGHT height) const
         {
-            return _allCards[color*4 + height];
+            return getInfo(color*8 + height);
         }
         void SetFallen(CARDS_COLOR color, CARDS_HEIGHT height)
         {
-            _allCards[color*4 + height] = true;
+            setInformation(color*8 + height);
+        }
+};
+class MemorizeCuts  : public BoolStorage<16> //efficient way to memorize which player cut where
+{
+    public :
+        MemorizeCuts(){}
+        const bool & IsFallen(POSITION_PLAYER player, CARDS_COLOR color) const
+        {
+            return getInfo(player*4 + color);
+        }
+        void SetFallen(POSITION_PLAYER player, CARDS_COLOR color)
+        {
+           setInformation(player*4 + color);
         }
 };
 
@@ -38,9 +66,21 @@ class AIGameMemory
 {
     protected :
         MemorizeCards _fallenCards;
+        MemorizeCuts _playerCut;
+        CARDS_HEIGHT _heightsMaster[4]; //key : color, stock the height of the cards master in the color
+
     public:
         AIGameMemory();
         virtual ~AIGameMemory();
+        void UpdateFullTrick(const std::array<Cards*,4>& trick)
+        {
+            CARDS_COLOR colorAsked = trick[0]->GetColour();
+            //update the playerCut
+            for(Uint i= 1; i < 4; i++)
+            {
+
+            }
+        }
     protected:
     private:
 };
